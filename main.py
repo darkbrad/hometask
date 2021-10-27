1f02164f48885095b4ee9a2e0c65ec9b87372eb7
denys <gleb.fomichov@gmail.com>
2021-10-27 16:32:45 +0300

    flask1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<name>")
+def hello_world(name):
+    return "<p>Hello,%s!</p>"%name
+if __name__=='__main__':
+    app.run(debug=True)
